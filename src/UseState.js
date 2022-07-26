22b5d1f0e1217242fe52276d9908221308680e64
Rmoreno3 <reynaldomoreno173@gmail.com>
2022-07-25 21:33:24 -0400

    Aplicando useEffect al estado de carga

--- src/UseState.js
+++ src/UseState.js
@@ -2,6 +2,23 @@ import React from 'react';
 
 function UseState(props) {
   const [error, setError] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
+
+  React.useEffect(() => {
+    console.log("Empezando el efecto");
+
+    if (!!loading) {
+      setTimeout(() => {
+        console.log("Haciendo la validacion");
+  
+        setLoading(false)
+  
+        console.log("Terminando la validacion");
+      }, 3000)
+    }
+
+    console.log("Terminando el efecto");
+  }, [loading])
 
   return (
     <div>
@@ -11,10 +28,13 @@ function UseState(props) {
       {error && (
         <p>Error: Codigo incorrecto.</p>
       )}
+      {loading && (
+        <p>Cargando...</p>
+      )}
 
       <input placeholder="Codigo de seguridad" />
       <button
-        onClick={() => setError(!error)}
+        onClick={() => setLoading(true)}
       >Comprobar</button>
     </div>
   )
